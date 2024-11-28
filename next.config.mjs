b951446b7fd8abeba231f2f3213dd06b9a63628f
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-11-28 11:42:08 +0700

    feat: add view movie page by movieId and add search and sorting feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-alpha-sig.figma.com",
+        pathname: "/img/**", // Adjust the path based on your needs
+      },
+      {
+        protocol: "https",
+        hostname: "i.ebayimg.com",
+        pathname: "/images/**", // Adjust the path if necessary
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-//Font test
+import { HStack } from "@chakra-ui/react";
+import { Button } from "@/components/ui/button";
+import { RiArrowRightLine, RiMailLine } from "react-icons/ri";
+import { Avatar, AvatarGroup } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
-    <div className="font-sans text-lg text-gray-800">
-      <h1 className="text-3xl font-bold">Test Font</h1>
-      <h2 className="text-3xl font-bold">The Dark Knight</h2>
-      <p className="font-extrabold text-5xl">All cinemas.</p>
-    </div>
+<h1></h1>
   );
 }
